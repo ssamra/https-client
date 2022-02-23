3c2fda23d34011495504c7b227dd202436bb0b86
Sukhwant Samra <sukhwant.sss@gmail.com>
2022-02-23 14:56:22 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const https = require('https');
+//const url = 'https://hvodc1.sse.codesandbox.io/';
+const url = 'https://nl6o88.sse.codesandbox.io/endpoint';
+
+https.get(url, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
